b98877b2220b89fab38b758e6e0308f72a3832ff
Cere <c_cere2909@hotmail.com>
2025-08-08 10:24:55 +0000

    modificado navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -6,12 +6,15 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">LOGO</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					<Link to="/" className="btn">Inicio</Link>
+					<Link to="/frases" className="btn">Frases</Link>
+					<Link to="/categorias" className="btn">Categorías</Link>
+					<Link to="/autores" className="btn">Autores</Link>
+					<Link to="/login" className="btn">Iniciar sesión</Link>
+					<Link to="/register" className="btn">Registrarse</Link>
 				</div>
 			</div>
 		</nav>
