031842de989fde4e5b46641fa2ed39cba7ef5b82
Cere <c_cere2909@hotmail.com>
2025-08-13 15:01:55 +0000

    modificado

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -14,13 +14,16 @@ export const Login = () => {
                     <label htmlFor="password" className="form-label">PASSWORD</label>
                     <input type="password" className="form-control" id="password"/>
                 </div>
-                <div className="mb-3 form-check d-flex justify-content-between">
-                    <div>
+
+                <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="rememberMe" />
                     <label className="form-check-label" htmlFor="rememberMe">Recordarme</label>
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-lg pe-5 ps-5">Acceder</button>
                 </div>
+
+                <div className="mb-3 text-center">
+                    <button type="submit" className="b5 btn btn-primary btn-lg pe-5 ps-5">Acceder</button>
+                </div>
+                
                 <div className="mt-4 d-flex justify-content-between">
                     <p className="text-center">¿No tienes cuenta? <a href="/register">Regístrate</a></p>
                     <p className="text-center">¿Has olvidado tu contraseña? <a href="/reset-password">Recuperar contraseña</a></p>
